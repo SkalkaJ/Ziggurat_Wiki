bc68807127eb189533af14ecc2a649f36df9deb9
Skalka <kubabuba.skalka@gmail.com>
2025-05-18 15:47:54 +0200

    Quartz sync: May 18, 2025, 3:47 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Ziggurat discord server": "https://discord.gg/YwSRJxNduV",
+      "Vytvoreno jako projekt Rhysem": "https://discord.gg/YwSRJxNduV",
     },
   }),
 }
